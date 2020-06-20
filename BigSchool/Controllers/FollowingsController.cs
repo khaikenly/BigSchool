a9916a317d6fedc21574a71d8e71bf26383ff5a6
Administrator <Administrator@PC272>
2020-06-20 11:08:26 +0700

    changes

--- BigSchool/Controllers/FollowingsController.cs
+++ BigSchool/Controllers/FollowingsController.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace BigSchool.Controllers
 {
-    public class FollowingsController : Controller
+    public class FollowingsController : ApiController
     {
-        // GET: Followings
-        public ActionResult Index()
-        {
-            return View();
-        }
+
     }
 }
